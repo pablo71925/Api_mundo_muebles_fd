60b4fc0f8da694c7ebc1a09b40eda78f92c4cbaa
Juan Pablo Fernandez <jean-paul99@hotmail.com>
2021-11-14 14:47:17 -0500

    PurchaserADD

--- src/main/java/com/mundomuebles/mundo_muebles_fd/domain/PurchaserDTO.java
+++ src/main/java/com/mundomuebles/mundo_muebles_fd/domain/PurchaserDTO.java
@@ -1,4 +1,32 @@
 package com.mundomuebles.mundo_muebles_fd.domain;
 
+
+import com.mundomuebles.mundo_muebles_fd.infrastructure.repositories.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class PurchaserDTO {
+    @NotNull
+    private int id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String email;
+    @NotNull
+    private String direction;
+    @NotNull
+    private int phone;
+    @NotNull
+    private LocalDate datePurchase;
+    @NotNull
+    private Product product;
+
 }
